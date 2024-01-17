d24467846b146107a3f0d5e5fc380d44bfdf4ad1
mukul soni <mukulsoni2593@gmail.com>
2024-01-17 17:51:40 -0500

    setup firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,11 @@ import {
   Form as BootstrapForm,
   Button,
 } from "react-bootstrap";
+import Header from "./Header";
+import { auth } from "../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+// import "../firebaseConfig"; // Add this line prevent firebase not loading error
+// import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 const initialValues = {
   email: "",
@@ -31,60 +36,69 @@ const validate = (values) => {
 };
 
 const LoginForm = () => {
-  const handleSubmit = (values, { setSubmitting }) => {
-    // You can handle login logic here
-    console.log(values);
+  // const db = getFirestore();
+  const submitForSignin = async (values, { setSubmitting }) => {
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then((userCred) => {
+        console.log(userCred.user.email);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     setSubmitting(false);
   };
 
   return (
-    <Container>
-      <Row className="justify-content-md-center mt-5">
-        <Col xs={12} md={6}>
-          <Formik
-            initialValues={initialValues}
-            validate={validate}
-            onSubmit={handleSubmit}
-          >
-            <Form>
-              <BootstrapForm.Group>
-                <BootstrapForm.Label>Email:</BootstrapForm.Label>
-                <Field
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="form-control"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-danger"
-                />
-              </BootstrapForm.Group>
+    <>
+      <Header />
+      <Container>
+        <Row className="justify-content-md-center mt-5">
+          <Col xs={12} md={6}>
+            <Formik
+              initialValues={initialValues}
+              validate={validate}
+              onSubmit={submitForSignin}
+            >
+              <Form>
+                <BootstrapForm.Group>
+                  <BootstrapForm.Label>Email:</BootstrapForm.Label>
+                  <Field
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-danger"
+                  />
+                </BootstrapForm.Group>
 
-              <BootstrapForm.Group>
-                <BootstrapForm.Label>Password:</BootstrapForm.Label>
-                <Field
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="form-control"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-danger"
-                />
-              </BootstrapForm.Group>
+                <BootstrapForm.Group>
+                  <BootstrapForm.Label>Password:</BootstrapForm.Label>
+                  <Field
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-danger"
+                  />
+                </BootstrapForm.Group>
 
-              <Button type="submit" variant="primary">
-                Login
-              </Button>
-            </Form>
-          </Formik>
-        </Col>
-      </Row>
-    </Container>
+                <Button type="submit" variant="primary">
+                  Login
+                </Button>
+              </Form>
+            </Formik>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,84 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import {
+  Container,
+  Row,
+  Col,
+  Form as BootstrapForm,
+  Button,
+} from "react-bootstrap";
+import Header from "./Header";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
-const Signup = () => {
-  return <div>Signup page</div>;
+const initialValues = {
+  email: "",
+  password: "",
 };
 
-export default Signup;
+const submitForSignup = async (values, { setSubmitting }) => {
+  createUserWithEmailAndPassword(auth, values.email, values.password)
+    .then((userCred) => {
+      console.log(userCred.user.email);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  setSubmitting(false);
+};
+
+const SignUp = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Row className="justify-content-md-center mt-5">
+          <Col xs={12} md={6}>
+            <Formik
+              initialValues={initialValues}
+              // validate={validate}
+              onSubmit={submitForSignup}
+            >
+              <Form>
+                <BootstrapForm.Group>
+                  <BootstrapForm.Label>Email:</BootstrapForm.Label>
+                  <Field
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-danger"
+                  />
+                </BootstrapForm.Group>
+
+                <BootstrapForm.Group>
+                  <BootstrapForm.Label>Password:</BootstrapForm.Label>
+                  <Field
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-danger"
+                  />
+                </BootstrapForm.Group>
+
+                <Button type="submit" variant="primary">
+                  SignUP
+                </Button>
+              </Form>
+            </Formik>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+export default SignUp;
