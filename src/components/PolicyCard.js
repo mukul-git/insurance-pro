60add09ef60bf53036656da875cd5a827de65144
mukul soni <mukulsoni2593@gmail.com>
2024-01-19 18:04:39 -0500

    updated json data

--- src/components/PolicyCard.js
+++ src/components/PolicyCard.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from "react-bootstrap";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
 
 const PolicyCard = () => {
   const [policies, setPolicies] = useState([]);
@@ -11,46 +20,92 @@ const PolicyCard = () => {
     };
     fetchData();
   }, [policies]);
-  // console.log(policies[0].health)
   return (
     <div>
-      { policies[0]?.health?.map((elem,index)=>{
-        return (
-<Card
-  color="light"
-  style={{
-    width: '18rem'
-  }}
->
-  <img
-    alt="Sample"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      {/* Card title */}
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      {/* Card subtitle */}
-    </CardSubtitle>
-    <CardText>
-      {elem}
-    </CardText>
-    <Button>
-      Button
-    </Button>
-  </CardBody>
-</Card>
-        )
-      } )}
+      <div
+        className="d-flex align-items-center justify-content-center text-center"
+        style={{
+          width: "100%", // Full width on small screens
+          height: "200px", // Fixed height for illustration (you can adjust as needed)
+          fontSize: "20vh",
+        }}
+      >
+        Featured Policies
+      </div>
+      <Row className="justify-content-center">
+        {policies?.type[0]?.health?.map((elem, index) => {
+          return (
+            <Col xs="12" sm="6" md="3" className="mb-4">
+              <Card
+                color="light"
+                style={{
+                  width: "18rem",
+                }}
+              >
+                <img alt="Sample" src="https://picsum.photos/300/200" />
+                <CardBody>
+                  <CardTitle tag="h5">{elem.title}</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    {/* Card subtitle */}
+                  </CardSubtitle>
+                  <CardText>{elem.description}</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+      <Row className="justify-content-center">
+        {policies?.type[1]?.life?.map((elem, index) => {
+          return (
+            <Col xs="12" sm="6" md="3" className="mb-4">
+              <Card
+                color="light"
+                style={{
+                  width: "18rem",
+                }}
+              >
+                <img alt="Sample" src="https://picsum.photos/300/200" />
+                <CardBody>
+                  <CardTitle tag="h5">{elem.title}</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    {/* Card subtitle */}
+                  </CardSubtitle>
+                  <CardText>{elem.description}</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+      <Row className="justify-content-center">
+        {policies?.type[2]?.property?.map((elem, index) => {
+          return (
+            <Col xs="12" sm="6" md="3" className="mb-4">
+              <Card
+                color="light"
+                style={{
+                  width: "18rem",
+                }}
+              >
+                <img alt="Sample" src="https://picsum.photos/300/200" />
+                <CardBody>
+                  <CardTitle tag="h5">{elem.title}</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    {/* Card subtitle */}
+                  </CardSubtitle>
+                  <CardText>{elem.description}</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 };
 
 export default PolicyCard;
-
-
-
